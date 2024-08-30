02b8caa60beb64d1c65f4ed9ba97b3f9b49a82da
Asish Kumar Sahu <meet2asishkumar@gmail.com>
2024-08-31 00:00:42 +0530

    FirstUniqueCharacterInAString.java

---
+++ FirstUniqueCharacterInAString.java
@@ -0,0 +1,20 @@
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+// Visit : https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int k;
+        for(int i=0;i<s.length();i++) {
+            k=0;
+            for(int j=0;j<s.length();j++) {
+                if(i==j) continue;
+                if(s.charAt(i)==s.charAt(j)) {
+                    k++; break;
+                }
+            }
+            if(k==0) return i;
+        }
+        return -1;
+    }
+}
