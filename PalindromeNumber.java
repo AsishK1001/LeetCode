ed65461dc9e48422fd624da6f21f8a2ff85584b1
AsishK1001 <meet2asishkumar@gmail.com>
2024-08-30 19:59:06 +0530

    PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,18 @@
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+//Visit : https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0,temp=x;
+        if(x<0) return false;
+        while(x!=0) {
+            rev=rev*10+(x%10);
+            x=x/10;
+        }
+        if ( rev == temp )
+            return true;
+        else
+            return false;
+    }
+}
